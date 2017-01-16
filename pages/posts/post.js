4505318c271a9d92157901ad29ae065a260189ec
江文得 <82697451@qq.com>
2017-01-16 11:48:58 +0800

    增加新闻列表页

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,19 +1,31 @@
 // pages/posts/post.js
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var processData = {
+      date: "2016-9-7",
+      post:{
+        title:"hello",
+        abstract:"this is zhaiyao"
+      }
+    }
+    this.setData(processData);
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
+    console.log("onReady");
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
+    console.log("onShow");
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
