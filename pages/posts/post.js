b542b51c010d86d0e9f8fa94c396474418850a21
江文得 <82697451@qq.com>
2017-01-17 17:29:42 +0800

    新闻列表数据绑定

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,31 +1,15 @@
-// pages/posts/post.js
+var postsData = require('../../data/posts-data.js');
+
 Page({
   data: {
-
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    var processData = {
-      date: "2016-9-7",
-      post:{
-        title:"hello",
-        abstract:"this is zhaiyao"
-      }
-    }
-    this.setData(processData);
+
+
+    this.setData({
+      posts_key: postsData.postList
+    });
   },
-  onReady: function () {
-    // 页面渲染完成
-    console.log("onReady");
-  },
-  onShow: function () {
-    // 页面显示
-    console.log("onShow");
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  }
+
 })
